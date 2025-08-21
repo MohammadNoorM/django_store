a69a3ec039fb591d94e20fba33e3eae162193add
mohammad noor <mohammadnoormerai@gmail.com>
2025-08-21 18:10:42 +0100

    connect with Frontent Template

--- store/urls.py
+++ store/urls.py
@@ -1,2 +1,13 @@
+from django.urls import path
+from . import views
+
+
 urlpatterns = [
+    path('', views.index, name='store.home'),
+    path('product/<int:product_id>/', views.product, name='store.product'),
+    path('category/<int:category_id>/', views.category, name='store.category'),
+    path('category/', views.category, name='store.category'),
+    path('cart/', views.cart, name='store.cart'),
+    path('checkout/', views.checkout, name='store.checkout'),
+    path('checkout/complete/', views.checkout_complete, name='store.checkout_complete'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def product(request, product_id):
+    return render(request, 'product.html')
+
+def category(request, category_id=None):
+    return render(request, 'category.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def checkout_complete(request):
+    return render(request, 'checkout-complete.html')
