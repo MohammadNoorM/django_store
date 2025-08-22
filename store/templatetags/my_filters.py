5abf01b454b2bb458948eefc188bac91ba5a1824
mohammad noor <mohammadnoormerai@gmail.com>
2025-08-22 14:17:27 +0100

    Upload content from database

---
+++ store/templatetags/my_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+def currency(value):
+    return f"{value} $"
+
+register.filter('currency', currency)
--- store/views.py
+++ store/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-
+from .models import Product, Slider, Category
+from django.core.paginator import Paginator
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.select_related('author').filter(featured=True)
+    slides = Slider.objects.order_by('order')
+    return render(request, 'index.html', {'products': products, 'slides': slides})
 
 
 def product(request, product_id):
-    return render(request, 'product.html')
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product.html', {'product': product})
 
 def category(request, category_id=None):
-    return render(request, 'category.html')
+    cat = None
+    where = {}
+    if category_id:
+        cat = Category.objects.get(id=category_id)
+        where['category_id'] = category_id
+    products = Product.objects.filter(**where)
+    paginator = Paginator(products, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'category.html',
+                  {'category': cat, 
+                   'page_obj': page_obj})
 
 def cart(request):
     return render(request, 'cart.html')
